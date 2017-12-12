658b2bab08238306711cf73d454984f57e252c04
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-12 16:30:51 +0100

    COrrection

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<?php
-$miles = 1;
-echo "$miles km";
-
-if ($miles >= 1) {
-  $miles += 2;
-  echo "\n $miles km";};
-if ($miles >= 1) {
-    $miles += 122;
-    echo "\n $miles km";
-};
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Partie 1 : exercice 3</title>
+  </head>
+  <body>
+    <?php
+    $miles = 1;
+    echo $miles . ' km';
+    ?>
+    <br/>
+    <?php
+    $miles = 3;
+    echo $miles . ' km';
+    ?>
+    <br/>
+    <?php
+    $miles = 125;
+    echo $miles . ' km';
+    ?>
+  </body>
+</html>
